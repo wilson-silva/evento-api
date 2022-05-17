fb675165f62570c6468d5a019f2f68efded2a9b7
Wilson <wilson-gs@hotmail.com>
2022-05-17 10:51:22 -0300

    criando repositorio para app

---
+++ src/main/java/com/brito/evento/models/Evento.java
@@ -0,0 +1,31 @@
+package com.brito.evento.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String Data;
+
+    @NotBlank
+    private String horário;
+}
