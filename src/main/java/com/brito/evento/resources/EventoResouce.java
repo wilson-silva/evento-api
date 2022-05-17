c2d9832ce61e8c2b6b22c2e55453a68fd8a29d83
Wilson <wilson-gs@hotmail.com>
2022-05-17 14:38:15 -0300

    finalizado implementacao de CRUD

--- src/main/java/com/brito/evento/resources/EventoResouce.java
+++ src/main/java/com/brito/evento/resources/EventoResouce.java
@@ -1,38 +1,57 @@
 package com.brito.evento.resources;
 
 import com.brito.evento.models.Evento;
-import com.brito.evento.repository.EventoRepository;
+import com.brito.evento.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/evento")
 public class EventoResouce {
 
     @Autowired
-    private EventoRepository eventoRepository;
+    private EventoService eventoService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody Iterable<Evento> listaEvento(){
-        return eventoRepository.findAll();
+    public List<Evento> listaEvento(){
+        return eventoService.listarEvento();
+    }
 
+    @GetMapping(value = "{codigo}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public  ResponseEntity<Evento> buscaEvento(@PathVariable Long codigo){
+        Evento evento = eventoService.buscarEvento(codigo);
+        return ResponseEntity.ok(evento);
     }
 
     @PostMapping()
-    public Evento cadastraEvento(@RequestBody @Valid Evento evento){
-        return eventoRepository.save(evento);
+    public ResponseEntity<Evento> cadastraEvento(@RequestBody @Valid Evento evento){
+        Evento eventoSalvo = eventoService.salvar(evento);
+        return ResponseEntity.ok(eventoSalvo);
     }
 
-    @DeleteMapping()
-    public Evento deletaEvento(@RequestBody Evento evento){
-        eventoRepository.delete(evento);
-        return evento;
-
+    @PutMapping(value = "{codigo}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Evento> alteraEvento(@RequestBody Evento evento, @PathVariable Long codigo){
+        try {
+            eventoService.atualizarEvento(evento, codigo);
+            return ResponseEntity.ok(evento);
+        }catch (RuntimeException ex){
+            return ResponseEntity.notFound().build();
+        }
     }
 
-
+    @DeleteMapping("{codigo}")
+    public ResponseEntity<Evento> deletaEvento(@PathVariable Long codigo){
+        try{
+            eventoService.excluirEvento(codigo);
+            return ResponseEntity.ok().build();
+        }catch (RuntimeException ex){
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
---
+++ src/main/java/com/brito/evento/service/EventoService.java
@@ -0,0 +1,43 @@
+package com.brito.evento.service;
+
+import com.brito.evento.models.Evento;
+import com.brito.evento.repository.EventoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventoService {
+
+    @Autowired
+    private EventoRepository eventoRepository;
+
+    public Evento salvar(Evento evento){
+        return eventoRepository.save(evento);
+    }
+
+    public List<Evento> listarEvento(){
+        return eventoRepository.findAll();
+    }
+
+    public Evento buscarEvento(Long codigo){
+        Optional<Evento> evento = eventoRepository.findById(codigo);
+        return evento.orElseThrow(() -> new EntityNotFoundException("Evento não encontrado"));
+    }
+
+    public Evento atualizarEvento(Evento evento, Long codigo){
+        Evento eventoOriginal = this.buscarEvento(codigo);
+        evento.setCodigo(eventoOriginal.getCodigo());
+        return eventoRepository.save(evento);
+    }
+
+    public void excluirEvento(Long codigo){
+        Evento eventoOriginal = this.buscarEvento(codigo);
+        eventoRepository.delete(eventoOriginal);
+    }
+
+
+}
