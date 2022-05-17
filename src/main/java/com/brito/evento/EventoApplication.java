385a1048a1709021ead25a70e07c102e4d781ba4
Wilson <wilson-gs@hotmail.com>
2022-05-17 15:47:35 -0300

    implementado swaggwer

--- src/main/java/com/brito/evento/EventoApplication.java
+++ src/main/java/com/brito/evento/EventoApplication.java
@@ -1,10 +1,12 @@
 package com.brito.evento;
 
-import com.brito.evento.models.Evento;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EventoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/brito/evento/resources/EventoResouce.java
+++ src/main/java/com/brito/evento/resources/EventoResouce.java
@@ -2,6 +2,8 @@ package com.brito.evento.resources;
 
 import com.brito.evento.models.Evento;
 import com.brito.evento.service.EventoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(value ="API REST EVENTOS")
 @RestController
 @RequestMapping("/evento")
 public class EventoResouce {
@@ -17,23 +20,27 @@ public class EventoResouce {
     @Autowired
     private EventoService eventoService;
 
+    @ApiOperation(value = "Retorna lista de eventos")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Evento> listaEvento(){
         return eventoService.listarEvento();
     }
 
+    @ApiOperation(value = "Busca um evento")
     @GetMapping(value = "{codigo}", produces = MediaType.APPLICATION_JSON_VALUE)
     public  ResponseEntity<Evento> buscaEvento(@PathVariable Long codigo){
         Evento evento = eventoService.buscarEvento(codigo);
         return ResponseEntity.ok(evento);
     }
 
-    @PostMapping()
+    @ApiOperation(value = "Cadastra um evento")
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Evento> cadastraEvento(@RequestBody @Valid Evento evento){
         Evento eventoSalvo = eventoService.salvar(evento);
         return ResponseEntity.ok(eventoSalvo);
     }
 
+    @ApiOperation(value = "Altera um evento")
     @PutMapping(value = "{codigo}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Evento> alteraEvento(@RequestBody Evento evento, @PathVariable Long codigo){
         try {
@@ -44,6 +51,7 @@ public class EventoResouce {
         }
     }
 
+    @ApiOperation(value = "Exclui um evento")
     @DeleteMapping("{codigo}")
     public ResponseEntity<Evento> deletaEvento(@PathVariable Long codigo){
         try{
